09326bf673f89e726c6891c9c1d770ce588dffad
yuvrajdiningtek <yuvraj@diningtek.com>
2023-07-22 19:04:58 +0530

    first commit

---
+++ TiffinApp/Offers.swift
@@ -0,0 +1,18 @@
+//
+//  Offers.swift
+//  TiffinApp
+//
+//  Created by NMX MacBook on 09/03/22.
+//  Copyright © 2022 YAY. All rights reserved.
+//
+
+import UIKit
+
+class Offers: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
